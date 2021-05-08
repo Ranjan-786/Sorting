1248a7678393754cb90b760c62f61f0a872a7ba3
ranjan kumar <ranjan.kumar_cs18@gla.ac.in>
2021-05-08 00:01:32 -0900

    done

--- QuickSort.java
+++ QuickSort.java
@@ -6,17 +6,17 @@ public class QuickSort
         QuickSort qs=new QuickSort();
         Scanner b=new Scanner (System.in);
         System.out.println("entre the length of array");
-        int n=b.nextInt();
+        int n=b.nextInt(); 
         System.out.println("entre the elements of array");
         int ar[]=new int[n];
         for(int i=0;i<=n-1;i++)
         {
             ar[i]=b.nextInt();
         } 
-       qs.quicksort(ar,0,n);
+       qs.quicksort(ar,0,n-1);
        for(int f :ar)
        {
-           System.out.println(f);
+           System.out.print(" "+f );
        }
     }
       
@@ -28,24 +28,37 @@ public class QuickSort
     }
     int partition(int ar[],int p,int q)
     {
-        int v=ar[p];
-        int j=p-1;
-        for(int i=0;i<=q-1;i++)
+      int v=ar[(p+q)/2];
+      while(p<=q)
+      {
+        while(v>ar[p])
         {
-            if(v>ar[i])
-            {
-                j++;
-                swap(ar,j,i);
-            }
+            p++;
         }
-        swap(ar,j+1,q);
-        return j+1;
+        while(v<ar[q])
+        {
+          q--;
+        }
+        if(p<=q)
+        {
+          swap(ar,p,q);
+          p++;
+          q--;
+        }
+      } 
+        return p;
     }
     void quicksort(int ar[],int p,int q)
-    {
+    {    
         int g =partition(ar,p,q);
-        quicksort(ar,p,g-1);
-        quicksort(ar, g+1, q);
+        if(p<g-1)
+        {   
+            quicksort(ar,p,g-1);
+        }
+        if(g<q)
+        {
+            quicksort(ar, g, q);
+        }
     }
     
 }
